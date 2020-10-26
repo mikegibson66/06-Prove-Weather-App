f401b47edd88e651c6aba4cf583e7c4c06ea502b
mikegibson66 <mikegibson66@byui.edu>
2020-10-26 16:25:29 -0600

    start project

---
+++ app/src/main/java/com/gibson/w06proveweatherapp/WeatherAppMain.java
@@ -0,0 +1,23 @@
+package com.gibson.w06proveweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class WeatherAppMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendCity(View view) {
+
+    }
+
+    public void sendForeCast(View view) {
+
+    }
+}
